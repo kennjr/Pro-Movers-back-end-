775d8c5add151bd595e784786377cc18784635a8
michael mapesa <michaelmapesa93@gmail.com>
2022-04-28 15:22:41 +0300

    add all fields in user model

--- backendapp/models.py
+++ backendapp/models.py
@@ -7,10 +7,17 @@ from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
 
 class User(AbstractBaseUser):
-    name = models.CharField(max_length=200, null=True)
+    full_name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True,null=True)
     bio = models.TextField(null=True)
-    avatar= models.ImageField(null=True, default='avatar.svg')
+    profile_img = models.ImageField(null=True, default='avatar.svg')
+    created_at=models.DateTimeField(auto_now_add=True)
+    username = models.CharField(max_length=50)
+    phone = models.IntegerField(null=True, blank=True)
+    location = models.CharField(max_length=100)
+    is_mover = models.BooleanField(default=False)
+
+
 
     USERNAME_FIELD='email'
     REQUIRED_FIELDS = []
