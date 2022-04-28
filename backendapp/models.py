78bf2258ee951c10fa1f9784b0aabbd47b8a16e6
michael mapesa <michaelmapesa93@gmail.com>
2022-04-28 13:55:25 +0300

    add media url

--- backendapp/models.py
+++ backendapp/models.py
@@ -1,4 +1,5 @@
 import email
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
@@ -9,9 +10,8 @@ class User(AbstractBaseUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True,null=True)
     bio = models.TextField(null=True)
+    avatar= models.ImageField(null=True, default='avatar.svg')
 
-    #   avatar
-    
     USERNAME_FIELD='email'
     REQUIRED_FIELDS = []
 
