81753ff6138451897a47a0d1f084d33d3b1210b1
michael mapesa <michaelmapesa93@gmail.com>
2022-04-28 13:29:59 +0300

    install pillow

--- backendapp/models.py
+++ backendapp/models.py
@@ -9,6 +9,10 @@ class User(AbstractBaseUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True,null=True)
     bio = models.TextField(null=True)
+
+    #   avatar
+    
     USERNAME_FIELD='email'
+    REQUIRED_FIELDS = []
 
 
