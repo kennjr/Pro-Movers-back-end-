09358d45a44fa77a7d28607588692a783bab2e47
kennjr <ken.juniour@student.moringaschool.com>
2022-05-04 00:29:15 +0300

    Second heroku commit, with updated allowed hosts list

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jzddvkt2t3z4b-li@z#oeq5hh#z%*(e6*hjpfrke7@)vc+98-m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
