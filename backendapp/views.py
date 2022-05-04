2766257183175879b46c5378e9ee5c23453fc29e
kennjr <ken.juniour@student.moringaschool.com>
2022-05-04 10:35:23 +0300

    Added the code for sending the email to the register view.

--- backendapp/views.py
+++ backendapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+# https://api-promovers.herokuapp.com/
+# usertwo@users.com token: aa25ca8f13c18c5ef66607558c4554ee3ec8813f
 
 # Create your views here.
 from rest_framework.authtoken.models import Token
@@ -9,6 +11,9 @@ from rest_framework.permissions import IsAuthenticated
 from .models import User
 from .serializers import UserSerializer
 
+from django.conf import settings
+from django.core.mail import send_mail
+
 
 @api_view(['POST'])
 def register_user(request):
@@ -16,8 +21,19 @@ def register_user(request):
     data = {}
     if serializer.is_valid(raise_exception=True):
         instance = serializer.save()
+
         if instance:
             token = Token.objects.get(user=instance).key
+            # The email section
+            subject = 'Welcome to ProMovers'
+            if instance.is_mover:
+                message = f'Hi {instance.username}, thank you for registering in as a user on ProMovers.'
+            else:
+                message = f'Hi {instance.username}, thank you for registering in as a mover on ProMovers. Where we will connect you to potential clients'
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [instance.email, ]
+            send_mail(subject, message, email_from, recipient_list)
+
             data['token'] = token
             data['response'] = "User registration, successful"
         else:
