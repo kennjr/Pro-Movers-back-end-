bdedeea2675d7b2d134b40e13d7374be444accca
kennjr <ken.juniour@student.moringaschool.com>
2022-05-05 08:04:52 +0300

    Added the path for getting a single user to the urlpatterns list

--- backendapp/urls.py
+++ backendapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # path('/', views)
     path('users/', views.api_get_all_users, name="all_users"),
     path('register/', views.register_user, name="register_user"),
-    path('login/', obtain_auth_token, name="login_user")
+    path('login/', obtain_auth_token, name="login_user"),
+    path('users/<int:uid>/', views.api_specific_user, name="one_user")
 ]
