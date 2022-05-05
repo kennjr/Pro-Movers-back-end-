deeecc5d196a33a77c6edcd8888b8f92a8f9953c
kennjr <ken.juniour@student.moringaschool.com>
2022-05-05 09:01:33 +0300

    Created the fun that'll get the validated data and save it to the db

--- backendapp/serializers.py
+++ backendapp/serializers.py
@@ -6,4 +6,13 @@ from backendapp.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'username', 'password']
+        fields = "__all__"
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
