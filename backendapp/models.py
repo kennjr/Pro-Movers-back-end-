286cd8582ca9db5b1a4fa6edba11f7613593ad27
michael mapesa <michaelmapesa93@gmail.com>
2022-05-07 10:20:41 +0300

    add general changes

--- backendapp/models.py
+++ backendapp/models.py
@@ -1,87 +1,111 @@
-import email
-from email.policy import default
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-from psycopg2 import Timestamp
+
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.core.validators import MaxValueValidator, MinValueValidator
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import BaseUserManager, PermissionsMixin
 
 
 # Create your models here.
-
-class User(AbstractBaseUser):
-    full_name = models.CharField(max_length=200,unique=True,null=True)
-    email = models.EmailField(unique=True,null=True)
-    bio = models.TextField(null=True,max_length=500)
-    profile_img = models.ImageField(null=True, default='avatar.svg')
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
-    username = models.CharField(max_length=50,null=True)
-    phone = models.IntegerField(null=True, blank=True)
-    location = models.CharField(max_length=100,null=True, blank=True)
-    is_mover = models.BooleanField(default=False)
+# https://app.getpostman.com/join-team?invite_code=f7cb4d2aa2b3e7e6587102b6766518f9
 
 
+class MyMgr(BaseUserManager):
 
-    USERNAME_FIELD='email'
+    def create_user(self, email, username, password=None):
+        user = self.model(
+            email=username,
+            username=email,
+            password=password
+        )
+        user.is_admin = False
+        user.is_staff = False
+        user.is_superuser = False
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password=None):
+        user = self.create_user(
+            username,
+            email,
+        )
+        user.set_password(password)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+    def get_by_natural_key(self, email_):
+        return self.get(email=email_)
+
+    def __str__(self):
+        return self.email
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        # "Does the user have a specific permission?"
+        return True
+
+    def has_module_perms(self, app_label):
+        # "Does the user have permissions to view the app 'app_label'?"
+        return True
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = MyMgr()
+
+    full_name = models.CharField(max_length=200, unique=False, null=True, blank=True)
+    email = models.EmailField(verbose_name='email', unique=True, null=False, db_index=True)
+    bio = models.TextField(null=True, max_length=500)
+    # profile_img = models.ImageField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    username = models.CharField(max_length=50, null=False, blank=False)
+    phone = models.CharField(null=True, blank=True, max_length=22)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    acc_type = models.CharField(max_length=7, default="user", null=False, blank=False)
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-class Email_msg(models.Model):
-    email_key = models.IntegerField(null=True)
-    to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sendingTo')
-    from1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receivingFrom')
-    timestamp = models.DateTimeField(auto_now_add=True, null=True)
 
-    def __str__(self):
-        return f'{self.email_msg} Email_msg'
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
+class Request(models.Model):
+    from_location = models.CharField(max_length=99, blank=False, null=False)
+    to_location = models.CharField(max_length=99, blank=False, null=False)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    user_id = models.IntegerField(blank=False, null=False)
+    mover_id = models.IntegerField(blank=False, null=False)
+    fees = models.IntegerField(default=5000, null=False, blank=False)
+    is_accepted = models.BooleanField(default=False,null=True,blank=True)
+    is_pending = models.BooleanField(default=False,null=True,blank=True)
+    is_declined = models.BooleanField(default=False,null=True,blank=True)
+    inventory = models.CharField(null=False, blank=False, max_length=500)
+    moving_time = models.CharField(max_length=66, null=False, blank=False)
+
+
+class Statistic(models.Model):
+    years_of_experience = models.IntegerField(blank=False, null=False)
+    no_of_movers = models.IntegerField(blank=False,null=False)
+    happy_clients = models.IntegerField(blank=False, null=False)
+    no_of_clients = models.IntegerField(blank=False, null=False)
+
     
-
-
-class Move(models.Model):
-    mover = models.ForeignKey(User, on_delete=models.CASCADE, related_name='move')
-    user =  models.ForeignKey(User, on_delete=models.CASCADE, related_name='Move')
-    scheduled_date = models.DateField((u"Conversation Date"),auto_now_add=True, blank=True)
-    created_at = models.DateField((u"Conversation Date"),auto_now_add=True, blank=True)
-
-    def __str__(self):
-        return f'{self.user.username} Move'
-
-    @receiver(post_save, sender=User)
-    def create_user_move(sender, instance, created, **kwargs):
-        if created:
-            Move.objects.create(user=instance)
-
-    @receiver(post_save, sender=User)
-    def save_user_move(sender, instance, **kwargs):
-        instance.move.save()
-
-    def save_move(self):
-        self.user
-
-    def delete_move(self):
-        self.delete()
-
-    @classmethod
-    def search_move(cls, name):
-        return cls.objects.filter(user__username__icontains=name).all()
-
-class Rating(models.Model):
-    comment = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True, null=True)
-    punctuality = models.BooleanField(default=False)
-    clumsy = models.BooleanField(default=False)
-    experience = models.IntegerField(default=0,
-        validators=[
-            MaxValueValidator(5),
-            MinValueValidator(0),
-        ]
-    )
-
-    def __str__(self):
-        return str(self.pk)
-
-
-
-
 
 
