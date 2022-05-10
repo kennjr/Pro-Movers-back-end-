eb17c48f7bbdd343a0430156892b2fada19afc72
kennjr <ken.juniour@student.moringaschool.com>
2022-05-10 14:43:33 +0300

    Added the urls for the mover queries

--- backendapp/urls.py
+++ backendapp/urls.py
@@ -1,11 +1,29 @@
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
-
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from . import views
 
+# for the swaggerUI
+# schema_view = get_swagger_view(title="ProMovers")
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ProMovers",
+      default_version='v1',
+      description="Documentation for the promovers API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="kennjunnior@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    # path('/', views)
+    # path('', schema_view),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('users/', views.api_get_all_users, name="all_users"),
+    path('movers/', views.api_get_all_movers, name="all_movers"),
     path('register/', views.register_user, name="register_user"),
     # path('login/', obtain_auth_token, name="login_user"),
     path('users/<str:username>/', views.api_specific_user, name="one_user"),
@@ -14,5 +32,6 @@ urlpatterns = [
     path('requests/<str:username>', views.api_get_all_users_requests, name="users_requests"),
     path('requests/', views.api_get_all_requests, name="all_requests"),
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('accounts/login/', views.MyTokenObtainPairView.as_view(), name="swagger_login"),
     path('login/refresh/', views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
